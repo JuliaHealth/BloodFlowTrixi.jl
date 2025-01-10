e287d0bc412a29f062652f65cf0cbf554661feb1
yolhan83 <yolhan@laposte.net>
2025-01-10 21:42:28 +0100

    fix doc

--- src/2DModel/2dmodel.jl
+++ src/2DModel/2dmodel.jl
@@ -51,6 +51,13 @@ function boundary_condition_outflow(u_inner, orientation_or_normal, direction, x
 end
 
 
+function boundary_condition_outflow(u_inner, orientation_or_normal, x, t, surface_flux_function, eq::BloodFlowEquations2D)
+    # Calculate the boundary flux without reflection
+    flux = surface_flux_function(u_inner, u_inner, orientation_or_normal, eq)
+    return flux
+end
+
+
 function boundary_condition_slip_wall(u_inner, orientation_or_normal, direction, x, t, surface_flux_function, eq::BloodFlowEquations2D)
     # Create the external boundary solution state with reflected normal velocity
     u_boundary = SVector(u_inner[1], -u_inner[2], u_inner[3], u_inner[4])
